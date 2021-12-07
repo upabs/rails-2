6ea03a3a32d8a47a56e29a20fb40109fbae54907
tu <phamtu.sn1@gmail.com>
2021-12-07 03:33:07 +0000

    add review book

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -1,6 +1,7 @@
 class BookReviewsController < ApplicationController
   before_action :set_book_review, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
+  
   # GET /book_reviews or /book_reviews.json
   def index
     @book_reviews = BookReview.all
@@ -22,7 +23,8 @@ class BookReviewsController < ApplicationController
   # POST /book_reviews or /book_reviews.json
   def create
     @book_review = BookReview.new(book_review_params)
-
+    @book_review.user_id = current_user.id
+    
     respond_to do |format|
       if @book_review.save
         format.html { redirect_to @book_review, notice: "Book review was successfully created." }
